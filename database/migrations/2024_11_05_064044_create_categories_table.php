53bb03858a6b5597bf62673b45b982ad8f302187
abubakar <abubakar@innovativeally.com>
2024-11-05 15:59:43 +0500

    working

--- database/migrations/2024_11_05_064044_create_categories_table.php
+++ database/migrations/2024_11_05_064044_create_categories_table.php
@@ -11,15 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->text('address')->nullable();
-            $table->string('phone_number')->nullable();
-            $table->enum('role', ['user', 'admin', 'seller'])->default('user');
-            $table->rememberToken();
+            $table->text('description')->nullable();
+            $table->foreignId('parent_category_id')->nullable()->constrained('categories');
             $table->timestamps();
         });
     }
@@ -29,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('categories');
     }
 };
